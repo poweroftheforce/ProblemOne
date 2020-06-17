1a7ad7b6b16ed0c0c6ad73751c3f606d14ccb569
Ashley E <poweroftheforce@gmsil.com>
2020-06-17 11:57:38 -0500

    Added logic to sort last name first, first name

--- index.js
+++ index.js
@@ -78,7 +78,21 @@ function mutateArray(a) {
       b.push(flattenObject(v))
     }
   });
-  return b;
+  return b.sort((a, b) => {
+    if (a.last_name > b.last_name) {
+      return 1;
+    }
+    if (a.last_name < b.last_name) {
+      return -1;
+    }
+    if (a.first_name > b.first_name) {
+      return 1;
+    }
+    if (a.first_name < b.first_name) {
+      return -1;
+    }
+    return 0;
+  });
 }
 
 $(document).ready(function() {
